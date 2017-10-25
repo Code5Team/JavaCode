e6e8d204b52a98cadfe8785531797a050c63ea0c
zhangyu0308 <510181251@qq.com>
2017-10-25 22:32:15 +0800

    快捷键注释

--- study/src/com/code5team/web/servlet/Zhangyu.java
+++ study/src/com/code5team/web/servlet/Zhangyu.java
@@ -1,5 +1,19 @@
 package com.code5team.web.servlet;
 
-public class Zhangyu {
 
+/**
+ * @author zhangyu
+ *JAVA 方法或者类的注释快捷键 ,方法： 1、先敲“/”在敲两个**，然后回车 ; 方法： 2、alt+shift+J
+ *方法： 1、先敲“/”在敲两个**，然后回车 ; 方法： 2、alt+shift+J
+ *Ctrl+1 提供快速重构方案
+ *
+ *Ctrl+Shift+O import进所调用的类
+ *ALT+/ 智能提示
+ *git地址：https://github.com/Code5Team/JavaCode.git
+ */
+public class Zhangyu {
+	public void firstDemo()
+	{
+		
+	}
 }
