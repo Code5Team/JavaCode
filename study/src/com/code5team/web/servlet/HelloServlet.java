63c78eaba3f25240d5cd1ef0e9addddfd879b007
zhangyu0308 <510181251@qq.com>
2017-10-25 22:14:25 +0800

    jhjhj

--- study/src/com/code5team/web/servlet/HelloServlet.java
+++ study/src/com/code5team/web/servlet/HelloServlet.java
@@ -3,17 +3,12 @@ package com.code5team.web.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class HelloServlet  {
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter pw = response.getWriter();
-		pw.println("ƒ„∫√ ¿ΩÁ");
-	}
+public void test()
+{
+	
+}
 
 }
